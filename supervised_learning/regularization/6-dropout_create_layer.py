035883b16281d5cbdecb884c93a13b3309aebc2f
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-05-31 19:41:03 +0200

    layer

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -7,9 +7,8 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """creating a layer"""
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    regu = tf.contrib.layers.l2_regularizer(lambtha)
+     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=init,
-                            kernel_regularizer=regu)
-    return layer(prev)
+                            kernel_initializer=init)
+    dropout = tf.layers.Dropout(rate=keep_prob)
+    return dropout(layer(prev))
