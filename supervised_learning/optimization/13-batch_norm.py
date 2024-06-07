3eed283c446cf565f6d843b9ed9851929e65ad30
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-06-07 16:40:34 +0200

    batch_norm

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Batch norm"""
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """normalizes an unactivated output of a neural network using batch normalization"""
+    mean = Z.mean(axis=1)
+    variance = Z.var(axis=1)
+    Z_norm = (Z - mean) / ((variance + epsilon) * 1/2)
+    Z_outpt = gamma * Z_norm + beta
+    return Z_outpt
