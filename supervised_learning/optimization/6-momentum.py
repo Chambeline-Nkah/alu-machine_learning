42cc096bd1cd07d2f4e487687214bf6dd8b9322d
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-06-07 10:07:29 +0200

    err

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -7,5 +7,5 @@ import tensorflow as tf
 
 def create_momentum_op(loss, alpha, beta1):
     """momentum using tf"""
-     optimizer = tf.train.MomentumOptimizer(alpha, beta1)
+    optimizer = tf.train.MomentumOptimizer(alpha, beta1)
     return optimizer.minimize(loss)
