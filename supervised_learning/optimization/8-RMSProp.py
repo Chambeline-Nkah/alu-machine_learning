4cbdd5b214170fd894f7d19b545094d080889333
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-06-07 16:50:02 +0200

    dc

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""RMSProp"""
+"""RMSProp optimization algorithm"""
 
 
 import tensorflow as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """RMSProp optimization algorithm"""
+    """Creating the training operation for a neural network in tensorflow using the RMSProp optimization algorithm"""
     optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
     return optimizer.minimize(loss)
