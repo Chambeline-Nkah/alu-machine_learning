4ede8bfbd1845a27fd99e5202de94fa5ccd0142e
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-06-07 16:53:46 +0200

    err:fix

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
-    """Creating the training operation for a neural network in tensorflow using the RMSProp optimization algorithm"""
-    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon)
+    """Creating the training operation for a neural
+    network in tensorflow using the RMSProp optimization algorithm"""
+    optimizer = tf.train.RMSPropOptimizer(alpha, beta2, epsilon=epsilon)
     return optimizer.minimize(loss)
