5c01b0f2893dadecede920c580d90752c26ba2f6
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-06-15 20:34:07 +0200

    ws

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -5,6 +5,7 @@ import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
+
 def f1_score(confusion):
     """Calculating the F1 score of a confusion matrix"""
     a = precision(confusion)
