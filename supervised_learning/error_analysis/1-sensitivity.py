502fef6b4f6a0741a057897e08d14254cfd2b752
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-06-15 20:23:33 +0200

    axis_er

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 def sensitivity(confusion):
     """Calculating the sensitivity for each class in a confusion matrix"""
-    return np.diag(confusion) / np.sum(confusion, axis=0)
+    return np.diag(confusion) / np.sum(confusion, axis=1)
