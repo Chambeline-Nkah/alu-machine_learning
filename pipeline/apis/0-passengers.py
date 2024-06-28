a7cd818a60ec23ad52d2dc988623be9449ff7bd4
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-06-29 00:26:44 +0200

    req

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Creating a method that returns the list of ships that can hold a given number of passengers"""
+
+import requests
+
+
+def availableShips(passengerCount):
+    """Returns a list of ships"""
+    url = https://swapi-api.alx-tools.com/api/
+    
+    req = requests.get(f'{url}/starship')
+
+    output = []
+    while req.status_code == 200:
+        req = req.json()
+        for ship in req['results']:
+            passengers = ship['passengers'].replace(',', '')
+            try:
+                if int(passengers) >= passengerCount:
+                    output.append(ship['name'])
+            except ValueError:
+                pass
+        try:
+            req = requests.get(req['next'])
+        except Exception:
+            break
+    return output
