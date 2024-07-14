98533ef18a5c06478d0c501d7bdb2c64ed7da6a0
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-07-14 17:33:18 +0200

    id

--- pipeline/databases/31-insert_school.py
+++ pipeline/databases/31-insert_school.py
@@ -4,5 +4,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """function that inserts a new document in a collection"""
-    new_document = mongo_collection.insert_one(kwargs)
+    new_document = mongo_collection.insert_one(kwargs).inserted_id
     return new_document
