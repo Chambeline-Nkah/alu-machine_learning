c814b7c96b6e22ccc0cc1d7b18847b2189da3132
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-09-06 21:40:37 +0200

    update init

--- supervised_learning/neural_style_transfer/3-neural_style.py
+++ supervised_learning/neural_style_transfer/3-neural_style.py
@@ -52,6 +52,7 @@ class NST:
         self.alpha = alpha
         self.beta = beta
         self.load_model()
+        self.generate_features()
 
     @staticmethod
     def scale_image(image):
@@ -134,18 +135,18 @@ class NST:
         content_feature - the content layer output of the content image
         """
         VGG19_model = tf.keras.applications.vgg19
-        #Pre-processing the style & content img
+        # Pre-processing the style & content img
         pp_s = VGG19_model.preprocess_input(
             self.style_image * 255)
         pp_c = VGG19_model.preprocess_input(
             self.content_image * 255)
 
-        style_fs = self.model(pp_s)[:-1]
-        content_f = self.model(pp_c)[-1]
+        style_features = self.model(pp_s)[:-1]
+        content_feature = self.model(pp_c)[-1]
 
-        gram_style_fs = []
-        for feature in style_fs:
-            gram_style_fs.append(self.gram_matrix(feature))
+        gram_style_features = []
+        for feature in style_features:
+            gram_style_features.append(self.gram_matrix(feature))
 
-        self.gram_style_features = gram_style_fs
-        self.content_feature = content_f
+        self.gram_style_features = gram_style_features
+        self.content_feature = content_feature
