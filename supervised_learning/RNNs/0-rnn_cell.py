e4f5591abf0c319297535f83aba057eda7052474
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-09-12 21:51:21 +0200

    docu

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-"""RNN"""
+"""Recurrent neural network"""
 
 import numpy as np
 
+
 class RNNCell:
     """class RNN
-    class constructor: def __init__(self, i, h, o)"""
+    class constructor: def __init__(self, i, h, o)
+    """
 
     def __init__(self, i, h, o):
         """i is the dimensionality of the data
         h is the dimensionality of the hidden state
         o is the dimensionality of the outputs
-
         Creates the public instance attributes Wh, Wy, bh, 
         by that represent the weights and biases of the cell
         Wh and bh are for the concatenated hidden state and input data
         Wy and by are for the output
-        The weights should be initialized using a random normal 
+        The weights should be initialized using a random normal
         distribution in the order listed above
         The weights will be used on the right side for matrix multiplication
         The biases should be initialized as zeros"""
@@ -25,7 +26,7 @@ class RNNCell:
         self.by = np.zeros((1, o))
         self.Wh = np.random.normal(size=(h + i, h))
         self.Wy = np.random.normal(size=(h, o))
-   
+
     # Defining the activation function to be used: softmax
 
     def softmax(self, x):
@@ -36,10 +37,9 @@ class RNNCell:
         softmax = fxn / fxn.sum(axis=1, keepdims=True)
         return softmax
 
-
     def forward(self, h_prev, x_t):
         """Performing forward propagation for one time step
-        - x_t is a numpy.ndarray of shape (m, i) that 
+        - x_t is a numpy.ndarray of shape (m, i) that
         contains the data input for the cell
         - m is the batche size for the data
         - h_prev is a numpy.ndarray of shape (m, h)
