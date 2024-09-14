e9d5ab61e4d5125698d88f253b612fcc08ce372f
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-09-14 13:13:58 +0200

    err_ws

--- supervised_learning/RNNs/6-bi_backward.py
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -63,7 +63,7 @@ class BidirectionalCell:
             h_next is a numpy.ndarray of shape (m, h)
             containing the next hidden state
             Returns: h_pev, the previous hidden state"""
- 
+
         h_x = np.concatenate((h_next, x_t), axis=1)
         h_prev = np.tanh(np.matmul(h_x, self.Whb) + self.bhb)
 
