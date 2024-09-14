dab72d120e80dd529a4655a18203da710a754ef1
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-09-14 22:58:40 +0200

    err

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -21,24 +21,24 @@ def deep_rnn(rnn_cells, X, h_0):
 
         Returns: H, Y
         H is a numpy.ndarray containing all of the hidden states
-        Y is a numpy.ndarray containing all of the outputs"""
-
-        layers = len(rnn_cells)
-        t, m, i = X.shape
-        l, m, h = h_0.shape
-        H = np.zeros((t + 1, layers, m, h))
-        H[0] = h_0
-        for step in range(t):
-            for layer in range(layers):
-                if layer == 0:
-                    h_prev = X[step]
-                h_prev, y = rnn_cells[layer].forward(H[step, layer], h_prev)
-                H[step + 1, layer, ...] = h_prev
-                if layer == layers - 1:
-                    if step == 0:
-                        Y = y
-                    else:
-                        Y = np.concatenate((Y, y))
-        output_shape = Y.shape[-1]
-        Y = Y.reshape(t, m, output_shape)
-        return (H, Y)
+        Y is a numpy.ndarray containing all of the outputs
+    """
+    layers = len(rnn_cells)
+    t, m, i = X.shape
+    l, m, h = h_0.shape
+    H = np.zeros((t + 1, layers, m, h))
+    H[0] = h_0
+    for step in range(t):
+        for layer in range(layers):
+            if layer == 0:
+                h_prev = X[step]
+            h_prev, y = rnn_cells[layer].forward(H[step, layer], h_prev)
+            H[step + 1, layer, ...] = h_prev
+            if layer == layers - 1:
+                if step == 0:
+                    Y = y
+                else:
+                    Y = np.concatenate((Y, y))
+    output_shape = Y.shape[-1]
+    Y = Y.reshape(t, m, output_shape)
+    return (H, Y)
