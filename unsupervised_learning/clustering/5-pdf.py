091ed472a58c5efb4d178e489007fe982f991a5b
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-10-19 19:03:33 +0200

    wl

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 def pdf(X, m, S):
-    """Function that calculates the probability density function of a Gaussian distribution
+    """Function that calculates the probability
+        density function of a Gaussian distribution
     X is a numpy.ndarray of shape (n, d)
         containing the data points whose PDF should be evaluated
     m is a numpy.ndarray of shape (d,)
@@ -14,7 +15,8 @@ def pdf(X, m, S):
         containing the covariance of the distribution
 
     Returns: P, or None on failure
-        P is a numpy.ndarray of shape (n,) containing the PDF values for each data point
+        P is a numpy.ndarray of shape (n,)
+            containing the PDF values for each data point
         All values in P should have a minimum value of 1e-300
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
