10ad7d9dce56810b68023a9e510a596639aeda3a
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-10-19 18:56:51 +0200

    cds

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -19,3 +19,12 @@ def initialize(X, k):
         S is a numpy.ndarray of shape (k, d, d) containing the covariance
             matrices for each cluster, initialized as identity matrices
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(k, int) or k < 1:
+        return None, None, None
+    _, d = X.shape
+    pi = np.ones(k) / k
+    m = kmeans(X, k)[0]
+    S = np.tile(np.identity(d), (k, 1, 1))
+    return pi, m, S
