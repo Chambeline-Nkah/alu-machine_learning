50ea9e97d3af973a04b38c779136045c7b675e27
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-10-26 13:12:56 +0200

    wlch

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -7,7 +7,7 @@ import numpy as np
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
     """Function that performs the Baum-Welch
         algorithm for a hidden markov model
-    
+
     Observations is a numpy.ndarray of shape (T,)
         that contains the index of the observation
     T is the number of observations
@@ -21,24 +21,35 @@ def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
         contains the initialized starting probabilities
     iterations is the number of times
         expectation-maximization should be performed
-    
+
     Returns: the converged Transition, Emission, or None, None on failure
     """
-    if not isinstance(Observations, np.ndarray) or Observations.ndim != 1:
-        return None, None
-    if Observations.shape[0] == 0:
-        return None, None
-    if not isinstance(Emission, np.ndarray) or Emission.ndim != 2:
-        return None, None
-    if not isinstance(Transition, np.ndarray) or Transition.ndim != 2:
-        return None, None
-    if Emission.shape[0] != Transition.shape[0]:
-        return None, None
-    if Transition.shape[0] != Transition.shape[1]:
-        return None, None
-    if not isinstance(Initial, np.ndarray) or Initial.ndim != 2:
-        return None, None
-    if Initial.shape[0] != Emission.shape[0] or Initial.shape[1] != 1:
+    if type(Observations) is not np.ndarray or len(Observations.shape) < 1:
         return None, None
 
+    T = Observations.shape[0]
+
+    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
+        return None, None
+
+    M, M_check = Transition.shape
+    if M != M_check:
+        return None, None
+
+    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
+        return None, None
+
+    M_check, N = Emission.shape
+    if M_check != M:
+        return None, None
+
+    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
+        return None, None
+
+    M_check, one = Initial.shape
+    if M_check != M or one != 1:
+        return None, None
+
+    if type(iterations) is not int or iterations < 1:
+        return None, None
     return None, None
