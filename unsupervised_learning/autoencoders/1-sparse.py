a824d838ff911802fe2474585cf4e4853353c6b6
Chambeline-Nkah <chambelinenkah57@gmail.com>
2024-11-02 00:04:40 +0200

    err

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -22,13 +22,18 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     regularizer = keras.regularizers.l1(lambtha)
 
     k = keras.layers
-    input = keras.Input(shape=(input_dims,))
-    encoded_layer = k.Dense(hidden_layers[0], activation='relu')(input)
-    for layer in hidden_layers[1:]:
-        encoded_layer = k.Dense(layer, activation='relu')(encoded_layer)
-    encoded_layer = k.Dense(latent_dims, activation='relu',
-                       activity_regularizer=regularizer)(encoded_layer)
-    encoder = keras.Model(input, encoded_layer)
+    encoder_inputs = keras.Input(shape=(input_dims,))
+    encoder_value = encoder_inputs
+    for i in range(len(hidden_layers)):
+        encoder_layer = keras.layers.Dense(units=hidden_layers[i],
+                                           activation='relu')
+        encoder_value = encoder_layer(encoder_value)
+    regularizer = keras.regularizers.l1(lambtha)
+    encoder_output_layer = keras.layers.Dense(units=latent_dims,
+                                              activation='relu',
+                                              activity_regularizer=regularizer)
+    encoder_outputs = encoder_output_layer(encoder_value)
+    encoder = keras.Model(inputs=encoder_inputs, outputs=encoder_outputs)
 
     coded_input = keras.Input(shape=(latent_dims,))
     decoded_layer = k.Dense(hidden_layers[-1], activation='relu')(coded_input)
